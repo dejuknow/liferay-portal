bf11962f09ca8b2ca191b15168893d8c135689a8
Shinn Lok <shinn.lok@liferay.com>
2015-02-12 16:25:43 -0800

    sync-engine - Switch to Carbon to avoid "Too many open files" exception

--- modules/util/sync-engine/src/com/liferay/sync/engine/filesystem/Watcher.java
+++ modules/util/sync-engine/src/com/liferay/sync/engine/filesystem/Watcher.java
@@ -73,7 +73,7 @@ public class Watcher implements Runnable {
 		_filePath = filePath;
 		_watchEventListener = watchEventListener;
 
-		if (OSDetector.isWindows()) {
+		if (OSDetector.isWindows() || OSDetector.isApple()) {
 			_recursive = false;
 		}
 		else {
@@ -388,7 +388,7 @@ public class Watcher implements Runnable {
 
 			fireWatchEventListener(eventType, filePath);
 
-			if (Files.isDirectory(filePath)) {
+			if (_recursive && Files.isDirectory(filePath)) {
 				walkFileTree(filePath);
 			}
 		}
